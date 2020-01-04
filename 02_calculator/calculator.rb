c0a86e65b9f1e63c69ea4fe454bf35447774d7e8
osama <hanhan.osama@gmail.com>
2020-01-04 01:26:15 -0800

    02 done

--- 02_calculator/calculator.rb
+++ 02_calculator/calculator.rb
@@ -1,1 +1,42 @@
 #write your code here
+
+def add(num_1, num_2)
+    num_1 + num_2
+end
+
+def subtract(num_1, num_2)
+    num_1 - num_2
+end
+
+def sum(array)
+    a = 0
+    array.each do |num|
+        a += num
+    end
+    a
+end
+
+def multiply(array) 
+    a = 1
+    array.each do |num|
+        a *= num
+    end
+    a
+end
+
+def power(num_1, num_2) 
+    num_1.pow(num_2)
+end
+
+
+def factorial(num_1)
+    n = 1
+
+    until num_1 < 1
+        n *= num_1        
+        num_1 -= 1
+    end
+    
+    n
+
+end
