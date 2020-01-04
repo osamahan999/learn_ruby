ac17cff64ac3f0cad0803bb25348466bda4e76a8
osama <hanhan.osama@gmail.com>
2020-01-03 16:24:19 -0800

    01 done

--- 01_temperature/temperature_spec.rb
+++ 01_temperature/temperature_spec.rb
@@ -4,7 +4,9 @@
 #
 # # Hints
 #
-# Remember that one degree fahrenheit is 5/9 of one degree celsius, and that the freezing point of water is 0 degrees celsius but 32 degrees fahrenheit.
+# Remember that one degree fahrenheit is 5/9 
+# of one degree celsius, and that the freezing point of
+# water is 0 degrees celsius but 32 degrees fahrenheit.
 #
 # In integer math, there **are no fractions**. So if you are using integer literals, you will be using integer math, which means, for example...
 #
@@ -13,7 +15,7 @@
 # In floating point math, there **are** fractions. So...
 #
 #    1.0 / 2.0 => 0.5
-#
+#s
 
 require "temperature"
 
