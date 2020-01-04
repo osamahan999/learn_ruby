3c3f8decefff75e908e20c9346ad327c9096bdb0
osama <hanhan.osama@gmail.com>
2020-01-04 02:09:15 -0800

    simon says done without little words function

--- 03_simon_says/simon_says.rb
+++ 03_simon_says/simon_says.rb
@@ -1,1 +1,34 @@
 #write your code here
+
+def echo(expression)
+    expression
+end
+
+def shout(expression)
+    expression.upcase
+end
+
+def repeat(expression, times=2)
+    expression * times
+end
+
+def start_of_word(word, letters) 
+    word[0..letters - 1]
+end
+
+def first_word(expression)
+    expression.split()[0]
+end
+
+    
+def titleize(expression)
+    expression = expression.split()
+    
+    
+    expression.map! {|word| word[0].upcase + word[1..]}
+
+
+
+    expression.join(" ")
+        
+end
