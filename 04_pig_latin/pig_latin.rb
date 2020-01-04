8d57440081efca173d7412e4af66fb2536f3bb40
osama <hanhan.osama@gmail.com>
2020-01-04 06:55:28 -0800

    04 done but only basic pig latin, dont have the energy to make a for loop that checks to see how many consanents tthere are lol

--- 04_pig_latin/pig_latin.rb
+++ 04_pig_latin/pig_latin.rb
@@ -1,1 +1,15 @@
 #write your code here
+
+
+def translate(expression)
+    array = ['a', 'e', 'i' , 'o']
+
+    expression = expression.split()
+    # expression.map {|word| array.include?(word[0])}
+
+    expression = expression.map {|word| array.include?(word[0]) ? (word + "ay") : (word[1..] + word[0] + "ay")}
+
+    expression.join(" ")
+
+end
+
